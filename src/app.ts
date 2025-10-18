8ca100630f1c50b0127cfa9c917936c6548501dc
PJ Lindsay <pjlindsay@gmail.com>
2025-10-18 16:44:16 -0600

    add submit handler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement
   hostElement: HTMLDivElement
   element: HTMLFormElement
+  titleInputElement: HTMLInputElement
+  descriptionInputElement: HTMLInputElement
+  peopleInputElement: HTMLInputElement
 
   constructor() {
     // hold reference to where I want template content to go
@@ -14,7 +17,26 @@ class ProjectInput {
     //import from template element and render to DOM
     const importedNode = document.importNode(this.templateElement.content, true)
     this.element = importedNode.firstElementChild as HTMLFormElement
+    this.element.id = 'user-input'
+
+    // get access to input elements and store them as properies of this class
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
+
+    this.configure()
     this.attach()
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+
+    // TODO: validate input
+    console.log(this.titleInputElement.value)
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this))
   }
 
   private attach() {
@@ -22,4 +44,4 @@ class ProjectInput {
   }
 }
 
-const prjInput = new ProjectInput();
+const prjInput = new ProjectInput()
