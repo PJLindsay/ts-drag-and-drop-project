4cbf5230d1fd57ce212512ed5bdb48a2fefe4acb
PJ Lindsay <pjlindsay@gmail.com>
2025-10-18 16:33:49 -0600

    Make Project form render

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 
+will make this as one large file
+then demonstrate how TS will allow us to restructure it cleaner
+
 npm start
 
 Seperate terminal: run TS compiler in watch mode
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement
+  hostElement: HTMLDivElement
+  element: HTMLFormElement
+
+  constructor() {
+    // hold reference to where I want template content to go
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement
+
+    this.hostElement = document.getElementById('app')! as HTMLDivElement
+
+    //import from template element and render to DOM
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLFormElement
+    this.attach()
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element)
+  }
+}
+
+const prjInput = new ProjectInput();
