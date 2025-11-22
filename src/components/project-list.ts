5180671303b358f8965ab3047e9a3876176f91ad
PJ Lindsay <pjlindsay@gmail.com>
2025-11-22 15:19:46 -0700

    Demo import and export syntaxes

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -2,7 +2,7 @@
 // note our imports must use js so it will work when compiled
 import { DragTarget } from '../models/drag-drop.js'
 import { Project, ProjectStatus } from '../models/project.js'
-import { Component } from './base-component.js'
+import Component from './base-component.js'
 import { autobind } from '../decorators/autobind.js'
 import { ProjectItem } from './project-item.js'
 import { projectState } from '../state/project-state.js'
