5ef512da28413a04d21a60d20224388c74e0da3a
PJ Lindsay <pjlindsay@gmail.com>
2025-11-22 14:42:50 -0700

    Organize Files and Folders

---
+++ src/components/project-item.ts
@@ -0,0 +1,53 @@
+/// <reference path="base-component.ts" />
+
+// ProjectItem Class
+namespace App {
+  export class ProjectItem
+    extends Component<HTMLUListElement, HTMLLIElement>
+    implements Draggable
+  {
+    private project: Project
+
+    get persons() {
+      if (this.project.people === 1) {
+        return '1 person'
+      } else {
+        return `${this.project.people} people`
+      }
+    }
+
+    constructor(hostId: string, project: Project) {
+      super('single-project', hostId, false, project.id)
+      this.project = project
+
+      this.configure()
+      this.renderContent()
+    }
+
+    // dataTransfer property is special for drag events
+    // this allows us to attach data to the drag event
+    // data will be stored during the drag and can be accessed on drop
+    @autobind
+    dragStartHandler(event: DragEvent) {
+      event.dataTransfer!.setData('text/plain', this.project.id)
+      // controls what the cursor will look like during drag
+      event.dataTransfer!.effectAllowed = 'move'
+    }
+
+    // Note: _ declares we won't use the parameter
+    dragEndHandler(_: DragEvent) {
+      console.log('DragEnd')
+    }
+
+    configure() {
+      this.element.addEventListener('dragstart', this.dragStartHandler)
+      this.element.addEventListener('dragend', this.dragEndHandler)
+    }
+
+    renderContent() {
+      this.element.querySelector('h2')!.textContent = this.project.title
+      this.element.querySelector('h3')!.textContent = this.persons + ' assigned'
+      this.element.querySelector('p')!.textContent = this.project.description
+    }
+  }
+}
---
+++ src/components/project-list.ts
@@ -0,0 +1,86 @@
+/// <reference path="base-component.ts" />
+
+// ProjectList Class
+namespace App {
+  export class ProjectList
+    extends Component<HTMLDivElement, HTMLElement>
+    implements DragTarget
+  {
+    assignedProjects: Project[]
+
+    constructor(private type: 'active' | 'finished') {
+      super('project-list', 'app', false, `${type}-projects`)
+      this.assignedProjects = []
+
+      this.configure()
+      this.renderContent()
+    }
+
+    @autobind
+    dragOverHandler(event: DragEvent) {
+      // we only allow dropping of plain text in this case
+      if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+        // default for JS drag and drop is to not allow dropping
+        // for this element we want to allow a drop when user lets go
+        event.preventDefault()
+        const listEl = this.element.querySelector('ul')!
+        // add CSS style to indicate it is droppable
+        listEl.classList.add('droppable')
+      }
+    }
+
+    @autobind
+    dropHandler(event: DragEvent) {
+      const projectId = event.dataTransfer!.getData('text/plain')
+      projectState.moveProject(
+        projectId,
+        this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished
+      )
+    }
+
+    @autobind
+    dragLeaveHandler(_: DragEvent) {
+      const listEl = this.element.querySelector('ul')!
+      // remove CSS style to indicate we're not dropping
+      listEl.classList.remove('droppable')
+    }
+
+    // Note: convention is to put PUBLIC methods above PRIVATE methods
+    configure() {
+      this.element.addEventListener('dragover', this.dragOverHandler)
+      this.element.addEventListener('dragleave', this.dragLeaveHandler)
+      this.element.addEventListener('drop', this.dropHandler)
+
+      projectState.addListener((projects: Project[]) => {
+        const relevantProjects = projects.filter((prj) => {
+          if (this.type === 'active') {
+            return prj.status === ProjectStatus.Active
+          }
+          return prj.status === ProjectStatus.Finished
+        })
+        this.assignedProjects = relevantProjects
+        this.renderProjects()
+      })
+    }
+
+    renderContent() {
+      const listId = `${this.type}-projects-list`
+      this.element.querySelector('ul')!.id = listId
+      this.element.querySelector('h2')!.textContent =
+        this.type.toUpperCase() + ' PROJECTS'
+    }
+
+    private renderProjects() {
+      const listEl = document.getElementById(
+        `${this.type}-projects-list`
+      )! as HTMLUListElement
+
+      // re-render all projects
+      listEl.innerHTML = ''
+      for (const projectItem of this.assignedProjects) {
+        // id of host element
+        new ProjectItem(this.element.querySelector('ul')!.id, projectItem)
+      }
+    }
+  }
+}
